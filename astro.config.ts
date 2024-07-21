cc69a3cd332dd44a8f130b29f2aa831119df9a90
Sungbin Lee <52542229+sungbinlee@users.noreply.github.com>
2024-07-21 12:58:38 +0900

    Revert "Update astro.config.ts"

--- astro.config.ts
+++ astro.config.ts
@@ -9,7 +9,6 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  base: 'sungbinlee.dev',
   integrations: [
     tailwind({
       applyBaseStyles: false,
